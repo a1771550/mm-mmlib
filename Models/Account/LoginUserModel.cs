2b4a21341a426bfb7ded5eee179a943d02c388aa
Kevin LAU <kevinlau@united.com.hk>
2024-05-31 12:15:19 +0800

    fixed reviewurl

--- Models/Account/LoginUserModel.cs
+++ Models/Account/LoginUserModel.cs
@@ -10,46 +10,30 @@ namespace MMLib.Models
 {
     public class LoginUserModel : SysUser
     {
-        //public ComInfo ComInfo { getPG; set; } //comInfo should be unknown before login!
-        public bool enableCRM { get; set; }
-        public string LoginMode { get; set; }
-        public bool IsPNG { get { return ConfigurationManager.AppSettings["IsPNG"] == "1"; } }
-
         public string SelectedShop { get; set; }
-
         public string SelectedDevice { get; set; }
-
-        public List<SelectListItem> ShopList { get; set; }
-        public List<SelectListItem> DeviceList { get; set; }
-
-        public List<AccountProfileView> AccountProfiles { get; set; }
         public string SelectedAccountProfile { get; set; }
         public SelectList AccountProfileList { get; set; }
-
-        public int iCentral { get; set; }
-        //public bool IsCentral { getPG { return ConfigurationManager.AppSettings["IsCentral"] == "1"; } }
         public string RedirectUrl { get; set; }
-        public List<SelectListItem> UserNameIdList { get; set; }
-   
-        public string SelectedEmail { get; set; }
         public string CompanyCode { get; set; }
-        public Dictionary<int, string> DicUserEmail { get; set; }
+
+        public MMDbContext DBContext { get { return new MMDbContext(); } }
 
         public LoginUserModel()
-        {           
-            AccountProfiles = new List<AccountProfileView>();
-            UserNameIdList = new List<SelectListItem>();
-            DicUserEmail = new Dictionary<int, string>();
-            using var context = new MMDbContext();
-            var users = context.SysUsers.Where(x=>x.AccountProfileId==AccountProfileId).ToList();
-            foreach(var user in users)
+        {
+        }
+
+        public LoginUserModel(string redirectUrl, int? superiorId = 0)
+        {
+            RedirectUrl = redirectUrl;
+            if (superiorId > 0)
             {
-                if (!DicUserEmail.ContainsKey(user.surUID))
+                using (DBContext)
                 {
-                    DicUserEmail[user.surUID] = user.Email;
+                    var user = DBContext.SysUsers.FirstOrDefault(x => x.surUID == superiorId);
+                    if (user != null) { HttpContext.Current.Session["User"] = user; Email = user.Email; }
                 }
             }
         }
-
     }
 }
